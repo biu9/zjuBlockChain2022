1c4191b1ae3f78d64ce8cc3275204f7e0fdc41cd
biu9 <1756127061@qq.com>
2022-10-24 17:21:21 +0800

    style: prettier code style

--- src/App.js
+++ src/App.js
@@ -3,60 +3,60 @@ import Controller from "./components/Controller";
 import { useEffect, useState } from "react";
 
 function checkCards() {
-    const triggerBottom = window.innerHeight / 5 * 4;
-    const cards = document.querySelectorAll("#card");
-    const cardChoice = document.querySelectorAll("#voteChoice");
-    cards.forEach((card,index) => {
-        //console.log(card.getBoundingClientRect().top);
-        if(card.getBoundingClientRect().top < triggerBottom) {
-            card.classList.add("translate-x-0");
-            card.classList.remove("translate-x-4full");
-            cardChoice[index].classList.add("translate-x-0");
-            cardChoice[index].classList.remove("translate-x-4full");
-        } else {
-            card.classList.remove("translate-x-0");
-            card.classList.add("translate-x-4full");
-            cardChoice[index].classList.remove("translate-x-0");
-            cardChoice[index].classList.add("translate-x-4full");
-        }
-    });
+	const triggerBottom = window.innerHeight / 5 * 4;
+	const cards = document.querySelectorAll("#card");
+	const cardChoice = document.querySelectorAll("#voteChoice");
+	cards.forEach((card, index) => {
+		//console.log(card.getBoundingClientRect().top);
+		if (card.getBoundingClientRect().top < triggerBottom) {
+			card.classList.add("translate-x-0");
+			card.classList.remove("translate-x-4full");
+			cardChoice[index].classList.add("translate-x-0");
+			cardChoice[index].classList.remove("translate-x-4full");
+		} else {
+			card.classList.remove("translate-x-0");
+			card.classList.add("translate-x-4full");
+			cardChoice[index].classList.remove("translate-x-0");
+			cardChoice[index].classList.add("translate-x-4full");
+		}
+	});
 }
 
 export default function App() {
-    window.addEventListener("scroll", checkCards);
-    const [currentCardIndex, setCurrentCardIndex] = useState(0);
-    const [currentCard, setCurrentCard] = useState(null);
-    const tmp = new Array(100).fill(0);
-    useEffect(() => {
-        const cards = document.querySelectorAll("#card");
-        cards.forEach((card,index) => {
-            if(index === currentCardIndex) {
-                card.classList.add("scale-125");
-                setCurrentCard(card);
-            } else {
-                card.classList.remove("scale-125");
-            }
-        });
-    },[currentCardIndex]);
-    return (
-        <div className="flex p-6 bg-stdBg min-h-screen flex-col items-center space-y-9 overflow-hidden">
-            <div className="font-mono text-3xl font-bold">
-                Voting Board
-            </div>
-            {
-                tmp.map((_,index) => {
-                    return (
-                        <Card
-                        key={index}
-                        />
-                    )
-                })
-            }
-            <Controller
-            currentCard={currentCard}
-            currentCardIndex={currentCardIndex}
-            setCurrentCardIndex={setCurrentCardIndex}
-            />
-        </div>
-    )
+	window.addEventListener("scroll", checkCards);
+	const [currentCardIndex, setCurrentCardIndex] = useState(0);
+	const [currentCard, setCurrentCard] = useState(null);
+	const tmp = new Array(100).fill(0);
+	useEffect(() => {
+		const cards = document.querySelectorAll("#card");
+		cards.forEach((card, index) => {
+			if (index === currentCardIndex) {
+				card.classList.add("scale-125");
+				setCurrentCard(card);
+			} else {
+				card.classList.remove("scale-125");
+			}
+		});
+	}, [currentCardIndex]);
+	return (
+		<div className="flex p-6 bg-stdBg min-h-screen flex-col items-center space-y-9 overflow-hidden">
+			<div className="font-mono text-3xl font-bold">
+				Voting Board
+			</div>
+			{
+				tmp.map((_, index) => {
+					return (
+						<Card
+							key={index}
+						/>
+					)
+				})
+			}
+			<Controller
+				currentCard={currentCard}
+				currentCardIndex={currentCardIndex}
+				setCurrentCardIndex={setCurrentCardIndex}
+			/>
+		</div>
+	)
 }
--- src/components/Controller/index.js
+++ src/components/Controller/index.js
@@ -1,19 +1,19 @@
 const ControllerBtnUp = ({ currentCardIndex, setCurrentCardIndex, currentCard }) => {
 	return (
-		<div 
-		onClick={() => {
-			const triggerBottom = currentCard.offsetHeight
-			const root = document.documentElement;
-			root.scrollTo({
-				top: root.scrollTop - triggerBottom,
-				behavior: "smooth"
-			});
-			if(currentCardIndex > 0) 
-				setCurrentCardIndex(currentCardIndex - 1);
-			currentCard.classList.remove("-translate-x-1/2");
-			currentCard.classList.remove("translate-x-1/2");
-		}}
-		className="bg-black w-20 h-20 rounded-xl flex justify-center items-center cursor-pointer">
+		<div
+			onClick={() => {
+				const triggerBottom = currentCard.offsetHeight
+				const root = document.documentElement;
+				root.scrollTo({
+					top: root.scrollTop - triggerBottom,
+					behavior: "smooth"
+				});
+				if (currentCardIndex > 0)
+					setCurrentCardIndex(currentCardIndex - 1);
+				currentCard.classList.remove("-translate-x-1/2");
+				currentCard.classList.remove("translate-x-1/2");
+			}}
+			className="bg-black w-20 h-20 rounded-xl flex justify-center items-center cursor-pointer">
 			<div className="bg-up w-10 h-10 bg-cover" />
 		</div>
 	)
@@ -21,12 +21,12 @@ const ControllerBtnUp = ({ currentCardIndex, setCurrentCardIndex, currentCard })
 
 const ControllerBtnLeft = ({ currentCardIndex, setCurrentCardIndex, currentCard }) => {
 	return (
-		<div 
-		onClick={() => {
-			currentCard.classList.remove("translate-x-1/2");
-			currentCard.classList.add("-translate-x-1/2");
-		}}
-		className="bg-black w-20 h-20 rounded-xl flex justify-center items-center cursor-pointer">
+		<div
+			onClick={() => {
+				currentCard.classList.remove("translate-x-1/2");
+				currentCard.classList.add("-translate-x-1/2");
+			}}
+			className="bg-black w-20 h-20 rounded-xl flex justify-center items-center cursor-pointer">
 			<div className="bg-up w-10 h-10 bg-cover transform -rotate-90" />
 		</div>
 	)
@@ -42,12 +42,12 @@ const ControllerBtnConfirm = ({ currentCardIndex, setCurrentCardIndex, currentCa
 
 const ControllerBtnRight = ({ currentCardIndex, setCurrentCardIndex, currentCard }) => {
 	return (
-		<div 
-		onClick={() => {
-			currentCard.classList.remove("-translate-x-1/2");
-			currentCard.classList.add("translate-x-1/2");
-		}}
-		className="bg-black w-20 h-20 rounded-xl flex justify-center items-center cursor-pointer">
+		<div
+			onClick={() => {
+				currentCard.classList.remove("-translate-x-1/2");
+				currentCard.classList.add("translate-x-1/2");
+			}}
+			className="bg-black w-20 h-20 rounded-xl flex justify-center items-center cursor-pointer">
 			<div className="bg-up w-10 h-10 bg-cover transform rotate-90" />
 		</div>
 	)
@@ -63,7 +63,7 @@ const ControllerBtnDown = ({ currentCardIndex, setCurrentCardIndex, currentCard 
 					top: root.scrollTop + triggerBottom,
 					behavior: "smooth"
 				});
-				if(currentCardIndex < 99)
+				if (currentCardIndex < 99)
 					setCurrentCardIndex(currentCardIndex + 1);
 				currentCard.classList.remove("-translate-x-1/2");
 				currentCard.classList.remove("translate-x-1/2");
